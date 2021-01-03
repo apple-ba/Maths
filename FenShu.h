4fbaa0f08a8e5e0703b0071c37315d4c6641bc9e
apple-ba <76874763+apple-ba@users.noreply.github.com>
2021-01-03 18:12:41 +0800

    Add files via upload

---
+++ FenShu.h
@@ -0,0 +1,54 @@
+/*
+	Name: FenShu.h
+	Copyright: dhc(c)2020
+	Author: Dong Haochen
+	Date: 25/12/20 09:43
+	Description: 分数支持
+*/
+
+#include"basic.h"
+
+struct FenShu
+{
+	UINT p, q;
+
+	bool neg;
+
+	void Correct()
+	{
+		unsigned int t = ZuiDaGongYueShu(p, q);
+		p /= t;
+		q /= t;
+	}
+
+	void FromDouble(double x)
+	{
+		if (x < 0)
+			neg = true;
+
+		q = 1;
+		while (int(x) != x)
+		{
+			x *= 10;
+			q *= 10;
+		}
+		Correct();
+	}
+
+	double ToDouble()
+	{
+		return 1.0 * p / q * (neg ? -1 : 1);
+	}
+};
+
+FenShu operator+(FenShu x, FenShu y)
+{
+	FenShu ans;
+
+	ans.q = ZuiXiaoGongBeiShu(x.q, y.q);
+	ans.p = x.p * ans.q + y.p * ans.q;
+	ans.Correct();
+
+	return ans;
+}
+
---
+++ basic.h
@@ -0,0 +1,21 @@
+
+typedef unsigned int UINT;
+
+UINT ZuiDaGongYueShu(UINT x, UINT y)
+{
+
+	while (x % y != 0 && y % x != 0) {
+		x %= y;
+		if (x % y != 0 && y % x != 0)
+			y %= x;
+	}
+
+	return (x > y) ? x % y : y % x;
+}
+
+
+UINT ZuiXiaoGongBeiShu(UINT x, UINT y)
+{
+	return x * y / ZuiDaGongYueShu(x, y);
+}
+
